722b735312f65a3715d17f0d82a832e5d354e2c5
FredXe <freddy115910810@gmail.com>
2024-06-17 05:48:56 +0800

    feat: add getCurrUser()

--- vue-js/src/lib/utils.js
+++ vue-js/src/lib/utils.js
@@ -1,6 +1,31 @@
+import axios from 'axios'
 import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
+
+export function wrapIntoArray(_data) {
+  if (!_data) return []
+  
+  if (_data instanceof Array) {
+    return _data
+  }
+
+  if (_data instanceof Object) {
+    return [_data]
+  }
+
+  return undefined
+}
+
+export function getCurrUser() {
+  axios.post('/api/account/login_get').then(result => {
+    if (!result.data.id) {
+      return undefined
+    }
+    return result.data
+  }).catch(console.error)
+}
+
